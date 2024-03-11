ae2674ae696adb457bc7fbd1bcebf02f5707d8ba
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-03-11 15:24:23 -0300

    create constructer

--- Flight.java
+++ Flight.java
@@ -2,5 +2,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Flight{
-    
+    private String flightNumber;
+    private String destination;
+    private int capacity;
+    private ArrayList<String> passengers;
+
+
+    public Flight(String flightNumber, String destination, int capacity, ArrayList<String> passengers) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = passengers;
+    }
 }
